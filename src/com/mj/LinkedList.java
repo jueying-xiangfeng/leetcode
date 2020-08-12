647efbfd94c4da5d8c99da076421e9cddf0fc01f
wangxiangfeng <jueying_xiangfeng@163.com>
2020-08-12 17:31:49 +0800

    leetcode

---
+++ src/com/mj/LinkedList.java
@@ -0,0 +1,100 @@
+package com.mj;
+
+import com.mj.AbstractList;
+
+@SuppressWarnings("unused")
+public class LinkedList<E> extends AbstractList<E> {
+	
+	private Node<E> first;
+	
+	// 内部类
+	private static class Node<E> {
+		E element;
+		Node<E> next;
+		public Node(E element, Node<E> next) {
+			this.element = element;
+			this.next = next;
+		}
+	}
+
+	@Override
+	public void clear() {
+		size = 0;
+		first = null;
+	}
+
+	@Override
+	public E get(int index) {
+		return node(index).element;
+	}
+
+	@Override
+	public E set(int index, E element) {
+		Node<E> node = node(index);
+		E old = node.element;
+		node.element = element;		
+		return old;
+	}
+
+	@Override
+	public void add(int index, E element) {
+		if (index == 0) {
+			first = new Node<>(element, first);
+		} else {
+			Node<E> preNode = node(index - 1);
+			preNode.next = new Node<>(element, preNode.next);
+		}
+		size++;
+	}
+
+	@Override
+	public E remove(int index) {
+		
+		Node<E> node = first;
+		if (index == 0) {
+			first = node.next;
+		} else {
+			Node<E> preNode = node(index - 1);
+			node = preNode.next;
+			preNode.next = node.next;
+		}
+		size--;
+		return node.element;
+	}
+
+	@Override
+	public int indexOf(E element) {
+		
+		if (element == null) {
+			Node<E> node = first;
+			for (int i = 0; i < size; i++) {
+				if (node.element == null) {
+					return i;
+				}
+				node = node.next;
+			}
+		} else {
+			Node<E> node = first;
+			for (int i = 0; i < size; i++) {
+				if (node.element.equals(element)) {
+					return i;
+				}
+				node = node.next;
+			}
+		}
+		return ELEMENT_NOT_FOUND;
+	}
+	
+	/**
+	 * 获取 index 对应 node
+	 */
+	private Node<E> node(int index) {
+		rangeCheck(index);
+		
+		Node<E> node = first;
+		for (int i = 0; i < index; i++) {
+			node = node.next;
+		}
+		return node;
+	}
+}
---
+++ src/com/mj/Main.java
@@ -0,0 +1,23 @@
+package com.mj;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.print("hello work!");
+		
+		System.out.println(123);
+		
+
+		ArrayList<Integer> list1 = new ArrayList<Integer>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+		list1.add(5);
+		
+		System.out.println(list1);
+		
+		
+		
+	}
+}
