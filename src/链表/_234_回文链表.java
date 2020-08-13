7575533cca53b3e7c8dad227eea4661724789467
wangxiangfeng <jueying_xiangfeng@163.com>
2020-08-13 19:10:47 +0800

    leetcode

---
+++ src/链表/_234_回文链表.java
@@ -0,0 +1,21 @@
+package 链表;
+
+/**
+ * https://leetcode-cn.com/problems/palindrome-linked-list/ 
+ * 
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 
+ * 输入: 1->2
+ * 输出: false
+ * 
+ */
+public class _234_回文链表 {
+	
+	
+    public boolean isPalindrome(ListNode head) {
+    	
+    	
+    	return true;
+    }
+}
