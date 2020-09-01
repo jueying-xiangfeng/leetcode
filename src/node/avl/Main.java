1ad1298ba281656658e1833a10a86fe76509e52b
wangxiangfeng <jueying_xiangfeng@163.com>
2020-09-01 18:23:46 +0800

    测试

--- src/node/avl/Main.java
+++ src/node/avl/Main.java
@@ -1,5 +1,10 @@
 package node.avl;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import node.avl.Times.Task;
 import node.tree.printer.BinaryTrees;
 
 public class Main {
@@ -27,6 +32,63 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		test1();
+//		test1();
+		
+		
+		List<Integer> data = new ArrayList<>();
+		for (int i = 0; i < 100_0000; i++) {
+			data.add((int)(Math.random() * 100_0000));
+		}
+		
+		List<Integer> addData = new ArrayList<>();
+		for (int i = 0; i < 100_0000; i++) {
+			addData.add((int)(Math.random() * 100_0000));
+		}
+		
+		// BST
+		BST<Integer> bst = new BST<Integer>();
+		for (int i = 0; i < data.size(); i++) {
+			bst.add(data.get(i));
+		}
+		
+		// AVL
+		AVLTree<Integer> avl = new AVLTree<Integer>();
+		for (int i = 0; i < data.size(); i++) {
+			avl.add(data.get(i));
+		}
+		
+		
+		Times.test("BST", new Task() {
+			
+			@Override
+			public void execute() {
+				for (int i = 0; i < data.size(); i++) {
+					bst.contains(data.get(i));
+				}
+				
+				
+//				for (int i = 0; i < data.size(); i++) {
+//					bst.remove(data.get(i));
+//				}
+			}
+		});
+		
+		
+		Times.test("AVL", new Task() {
+			
+			@Override
+			public void execute() {
+				
+				for (int i = 0; i < data.size(); i++) {
+					avl.contains(data.get(i));
+				}
+				
+				
+//				for (int i = 0; i < data.size(); i++) {
+//					avl.remove(data.get(i));
+//				}
+			}
+		});
+		
 	}
 }
